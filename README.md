d62e0eb9c5d8a6e375640739e3e49b77b57ded54
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-02-21 17:02:38 -0500

    Iniciando proyecto

---
+++ README.md
@@ -0,0 +1,44 @@
+## Proyecto 1 introducción a la ciencia de los datos
+
+### Integrantes:
+
+- Juan Esteban López Aránzazu - 202313026
+- Daniel Meléndez Ramirez - 202313024
+- Joan Sebastián Saavedra Perafán - 202313025
+
+### Descripción:
+
+1. Diseño del Modelo de la Bodega de Datos
+2. Extracción, Transformación y Carga de Datos
+3. Consultas Analíticas en SQL
+4. Análisis Descriptivo y Visualización de Datos
+
+### Entorno virtual e instalar dependencias
+
+Para instalar las dependencias especificadas en `requirements.txt`, abre una terminal y navega hasta el directorio del proyecto. Luego, ejecuta el siguiente comando:
+
+Para el entorno virtual en linux
+```bash
+python3 -m venv venv
+source venv/bin/activate # Para activar el entorno virtual
+deactivate # Para desactivar el entorno virtual
+```
+
+Para el entorno virtual en windows
+```bash
+python -m venv venv
+.\venv\Scripts\activate # Para activar el entorno virtual
+.\venv\Scripts\deactivate # Para desactivar el entorno virtual
+```
+
+Para instalar dependencias
+```bash
+pip install -r requirements.txt
+```
+
+### Ejecución del programa
+
+Para correr el programa puedes ejecutar el siguiente comando:
+```bash
+python main.py
+```
---
+++ sql/create_tables.sql
@@ -0,0 +1,54 @@
+-- crear bodega de datos
+
+CREATE DATABASE IF NOT EXISTS data_warehouse;
+USE data_warehouse;
+
+-- tablas de dimensiones
+
+CREATE TABLE IF NOT EXISTS dim_customer  (
+  id VARCHAR PRIMARY KEY,
+  gender VARCHAR,
+  age INT
+);
+
+CREATE TABLE IF NOT EXISTS dim_category  (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS dim_payment_method  (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS dim_shopping_mall  (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS dim_date  (
+  id SERIAL PRIMARY KEY,
+  full_date DATE,
+  day INT,
+  month INT,
+  year INT,
+  day_of_week INT
+);
+
+-- tablas de hechos
+
+CREATE TABLE IF NOT EXISTS fact_invoice  (
+  id VARCHAR PRIMARY KEY,
+  customer_id VARCHAR,
+  category_id INT,
+  payment_method_id INT,
+  shopping_mall_id INT,
+  date_id INT,
+  price DECIMAL(10, 2),
+  quantity INT,
+  FOREIGN KEY (customer_id) REFERENCES dim_customer(id),
+  FOREIGN KEY (category_id) REFERENCES dim_category(id),
+  FOREIGN KEY (payment_method_id) REFERENCES dim_payment_method(id),
+  FOREIGN KEY (shopping_mall_id) REFERENCES dim_shopping_mall(id),
+  FOREIGN KEY (date_id) REFERENCES dim_date(id)
+);
